58ece00ff38d05b9fa15622b127bbd3b17aca949
zlubsen <zeeger@lubsen.eu>
2024-01-28 14:47:45 +0100

    Attribute internal consistency tests

--- dis-rs/src/common/attribute/mod.rs
+++ dis-rs/src/common/attribute/mod.rs
@@ -1,3 +1,39 @@
 pub mod model;
 pub mod parser;
 pub mod writer;
+
+#[cfg(test)]
+mod tests {
+    use bytes::BytesMut;
+    use crate::common::attribute::model::Attribute;
+    use crate::enumerations::{PduType};
+    use crate::common::model::{Pdu, PduHeader};
+    use crate::common::parser::parse_pdu;
+    use crate::common::Serialize;
+    use crate::common::model::{DisTimeStamp};
+
+    #[test]
+    fn attribute_internal_consistency() {
+        let header = PduHeader::new_v6(1, PduType::Attribute);
+
+        let body = Attribute::default()
+            .into_pdu_body();
+        let original_pdu = Pdu::finalize_from_parts(header, body, DisTimeStamp::new_absolute_from_secs(100));
+        let pdu_length = original_pdu.header.pdu_length;
+
+        let mut buf = BytesMut::with_capacity(pdu_length as usize);
+
+        original_pdu.serialize(&mut buf);
+
+        let parsed = parse_pdu(&buf);
+        match parsed {
+            Ok(ref pdu) => {
+                assert_eq!(&original_pdu, pdu);
+            }
+            Err(ref _err) => {
+                println!("{_err}");
+                assert!(false);
+            }
+        }
+    }
+}
