2f9c9c7bc0b6fa75c3588f3a541d62d7d6e770fa
zlubsen <zeeger@lubsen.eu>
2024-03-08 14:26:00 +0100

    Added IsGroupOf PDU

--- dis-rs/src/common/is_group_of/mod.rs
+++ dis-rs/src/common/is_group_of/mod.rs
@@ -6,18 +6,43 @@ pub mod builder;
 #[cfg(test)]
 mod tests {
     use bytes::BytesMut;
-    use crate::enumerations::{PduType};
+    use crate::enumerations::{AirPlatformAppearance, IsGroupOfGroupedEntityCategory, PduType};
     use crate::common::model::{Pdu, PduHeader};
     use crate::common::parser::parse_pdu;
     use crate::common::Serialize;
     use crate::common::model::{DisTimeStamp};
-    use crate::is_group_of::model::IsGroupOf;
+    use crate::entity_state::model::EntityAppearance;
+    use crate::is_group_of::model::{GEDRecord7, GEDRecord8, GroupEntityDescription, GroupReferencePoint, IsGroupOf};
+    use crate::model::EntityId;
 
     #[test]
     fn is_group_of_internal_consistency() {
         let header = PduHeader::new_v6(1, PduType::IsGroupOf);
 
         let body = IsGroupOf::builder()
+            .with_group_id(EntityId::new(1, 10, 20))
+            .with_group_reference_point(GroupReferencePoint::default()
+                .with_latitude(1.0)
+                .with_longitude(1.0))
+            .with_grouped_entity_category(IsGroupOfGroupedEntityCategory::EnhancedFixedWingAircraft)
+            .with_description(GroupEntityDescription::EnhancedFixedWingAircraft(
+                GEDRecord8 {
+                    basic_fixed_wing_aircraft: GEDRecord7 {
+                        entity_id: 30,
+                        location: Default::default(),
+                        appearance: EntityAppearance::AirPlatform(AirPlatformAppearance::default()),
+                        orientation: Default::default(),
+                        fuel_status: 0,
+                        movement_horizontal_deviation: 0,
+                        movement_vertical_deviation: 0,
+                        movement_speed: 0,
+                    },
+                    supplemental_fuel_status: 1,
+                    air_maintenance_status: 2,
+                    primary_ammunition: 3,
+                    secondary_ammunition: 4,
+                }
+            ))
 
             .build()
             .into_pdu_body();
