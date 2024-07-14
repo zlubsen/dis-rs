264d5702e2cdf73c4f60131af82fb13b2a154368
zlubsen <zeeger@lubsen.eu>
2024-07-14 14:29:19 +0200

    C-DIS: EventReport fix in parser

--- cdis-assemble/src/event_report/parser.rs
+++ cdis-assemble/src/event_report/parser.rs
@@ -4,8 +4,7 @@ use nom::multi::count;
 use dis_rs::model::DatumSpecification;
 use crate::{BodyProperties, CdisBody, parsing};
 use crate::constants::{TWO_BITS};
-use crate::data::model::{DataFieldsPresent};
-use crate::event_report::model::EventReport;
+use crate::event_report::model::{EventReport, EventReportFieldsPresent};
 use crate::parsing::BitInput;
 use crate::records::parser::{entity_identification, fixed_datum, variable_datum};
 use crate::types::parser::{uvint32, uvint8};
@@ -18,10 +17,10 @@ pub(crate) fn event_report_body(input: BitInput) -> IResult<BitInput, CdisBody> 
     let (input, event_type) = uvint32(input)?;
 
     let (input, number_of_fixed_datums) = parsing::parse_field_when_present(
-        fields_present, DataFieldsPresent::FIXED_DATUMS_BIT, uvint8)(input)?;
+        fields_present, EventReportFieldsPresent::FIXED_DATUMS_BIT, uvint8)(input)?;
     let number_of_fixed_datums = parsing::varint_to_type::<_, _, usize>(number_of_fixed_datums);
     let (input, number_of_var_datums) = parsing::parse_field_when_present(
-        fields_present, DataFieldsPresent::VARIABLE_DATUMS_BIT, uvint8)(input)?;
+        fields_present, EventReportFieldsPresent::VARIABLE_DATUMS_BIT, uvint8)(input)?;
     let number_of_var_datums = parsing::varint_to_type::<_, _, usize>(number_of_var_datums);
 
     let (input, fixed_datums) = if let Some(num_datums) = number_of_fixed_datums {
