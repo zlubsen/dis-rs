d3d3afeedd463663a1788d3962a41c22e5af28f0
zlubsen <zeeger@lubsen.eu>
2025-01-20 21:59:33 +0100

    Created macro for NodeData implementations, avoid boilerplate

--- gateway-core/src/modules/dis.rs
+++ gateway-core/src/modules/dis.rs
@@ -4,6 +4,7 @@ use crate::core::{
     DEFAULT_OUTPUT_STATS_INTERVAL_MS,
 };
 use crate::error::InfraError;
+use crate::node_data_impl;
 use crate::runtime::{Command, Event};
 use bytes::{Bytes, BytesMut};
 use dis_rs::enumerations::ProtocolVersion;
@@ -119,41 +120,14 @@ impl NodeData for DisRxNodeData {
         })
     }
 
-    fn request_subscription(&self) -> Box<dyn Any> {
-        let client = self.outgoing.subscribe();
-        Box::new(client)
-    }
-
-    fn register_subscription(&mut self, receiver: Box<dyn Any>) -> Result<(), InfraError> {
-        if let Ok(receiver) = receiver.downcast::<Receiver<Bytes>>() {
-            self.incoming = Some(*receiver);
-            Ok(())
-        } else {
-            Err(InfraError::SubscribeToChannel {
-                instance_id: self.base.instance_id,
-                node_name: self.base.name.clone(),
-                data_type_expected: "Bytes".to_string(),
-            })
-        }
-    }
-
-    fn request_external_sender(&mut self) -> Result<Box<dyn Any>, InfraError> {
-        let (incoming_tx, incoming_rx) = channel::<Bytes>(DEFAULT_NODE_CHANNEL_CAPACITY);
-        self.register_subscription(Box::new(incoming_rx))?;
-        Ok(Box::new(incoming_tx))
-    }
-
-    fn id(&self) -> InstanceId {
-        self.base.instance_id
-    }
-
-    fn name(&self) -> &str {
-        self.base.name.as_str()
-    }
-
-    fn spawn_into_runner(self: Box<Self>) -> Result<JoinHandle<()>, InfraError> {
-        DisRxNodeRunner::spawn_with_data(*self)
-    }
+    node_data_impl!(
+        Bytes,
+        self.incoming,
+        self.outgoing,
+        self.base.instance_id,
+        self.base.name,
+        DisRxNodeRunner
+    );
 }
 
 impl NodeRunner for DisRxNodeRunner {
@@ -289,41 +263,14 @@ impl NodeData for DisTxNodeData {
         })
     }
 
-    fn request_subscription(&self) -> Box<dyn Any> {
-        let client = self.outgoing.subscribe();
-        Box::new(client)
-    }
-
-    fn register_subscription(&mut self, receiver: Box<dyn Any>) -> Result<(), InfraError> {
-        if let Ok(receiver) = receiver.downcast::<Receiver<Pdu>>() {
-            self.incoming = Some(*receiver);
-            Ok(())
-        } else {
-            Err(InfraError::SubscribeToChannel {
-                instance_id: self.base.instance_id,
-                node_name: self.base.name.clone(),
-                data_type_expected: "Pdu".to_string(),
-            })
-        }
-    }
-
-    fn request_external_sender(&mut self) -> Result<Box<dyn Any>, InfraError> {
-        let (incoming_tx, incoming_rx) = channel::<Pdu>(DEFAULT_NODE_CHANNEL_CAPACITY);
-        self.register_subscription(Box::new(incoming_rx))?;
-        Ok(Box::new(incoming_tx))
-    }
-
-    fn id(&self) -> InstanceId {
-        self.base.instance_id
-    }
-
-    fn name(&self) -> &str {
-        self.base.name.as_str()
-    }
-
-    fn spawn_into_runner(self: Box<Self>) -> Result<JoinHandle<()>, InfraError> {
-        DisTxNodeRunner::spawn_with_data(*self)
-    }
+    node_data_impl!(
+        Pdu,
+        self.incoming,
+        self.outgoing,
+        self.base.instance_id,
+        self.base.name,
+        DisTxNodeRunner
+    );
 }
 
 impl NodeRunner for DisTxNodeRunner {
