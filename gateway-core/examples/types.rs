dc199f25e7776100a19054457e05343a80bd05b0
zlubsen <zeeger@lubsen.eu>
2025-02-03 20:16:23 +0100

    Actions: formatting thing

--- gateway-core/examples/types.rs
+++ gateway-core/examples/types.rs
@@ -6,10 +6,12 @@ fn main() {
     println!("tx: {:?}, rx: {:?}", tx.type_id(), rx.type_id());
     let boxed_tx = Box::new(tx.clone());
     println!("boxed_tx: {:?}", boxed_tx.type_id());
-    let dyn_boxed_tx : Box<dyn Any> = Box::new(tx.clone());
+    let dyn_boxed_tx: Box<dyn Any> = Box::new(tx.clone());
     println!("dyn_boxed_tx: {:?}", dyn_boxed_tx.type_id());
 
     if let Ok(downcast_tx) = dyn_boxed_tx.downcast::<tokio::sync::broadcast::Sender<u8>>() {
         println!("downcast_tx: {:?}", downcast_tx.type_id());
-    } else { println!("Nope"); }
+    } else {
+        println!("Nope");
+    }
 }
