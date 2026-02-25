ce4d93c0d7efa74f67cc9756b4b048ee2f7fda08
zlubsen <zeeger@lubsen.eu>
2026-02-25 12:28:25 +0100

    Experimenting with intermediate representation to generated code structures.

--- dis-rs/build/siso_1278_v8/generation.rs
+++ dis-rs/build/siso_1278_v8/generation.rs
@@ -1,7 +1,6 @@
 use super::GenerationItem;
 use proc_macro2::{Ident, TokenStream};
 use quote::{format_ident, quote};
-use std::str::FromStr;
 
 // Module tree of generated sources:
 // src/v8
@@ -22,6 +21,46 @@ use std::str::FromStr;
 //            parser.rs
 //            writer.rs
 
+// Approach
+// Construct a tree structure, where each node represents a code unit (module, struct, enum, function, impl block).
+// Each node in the tree contains the children contained in that code unit. E.g modules, which are parents, and Structs, impl blocks, etc are the leafs.
+// Tree construction is an intermediate step before code generation.
+// 1. Create a Vec<(String, TokenStream)>, filled with all pieces of generated code.
+// 2. Walk the tree and concat all produced TokenStreams into one.
+// 3. Write the final code into the output file.
+
+struct Tree {
+    next_id: usize,
+    root: Vec<Node>,
+}
+
+impl Tree {
+    pub fn new() -> Self {
+        Self {
+            next_id: 0,
+            root: vec![],
+        }
+    }
+
+    pub fn new_with_root(root: Node) -> Self {
+        Self {
+            next_id: 1,
+            root: vec![root],
+        }
+    }
+}
+
+struct Node {
+    id: usize,
+    item: NodeItem,
+    children: Vec<usize>,
+}
+
+enum NodeItem {
+    Module(String),
+    Unit(GenerationItem),
+}
+
 pub fn generate(items: &[GenerationItem]) -> TokenStream {
     let families = vec![
         "acoustic",
@@ -29,7 +68,7 @@ pub fn generate(items: &[GenerationItem]) -> TokenStream {
         "ee",
         "entity_info_interaction",
     ];
-    // let mut generated_items = vec![];
+
     let generated = generate_v8_module(&families);
 
     println!("{generated}");
@@ -43,26 +82,36 @@ fn generate_v8_module(families: &Vec<&str>) -> TokenStream {
         .map(|&family| format_ident!("{family}"))
         .collect();
 
-    let common_mod = generate_common_module();
+    let contents = quote!(
+        pub const HELLO_NAMED: &str = "Hello from a module";
+    );
+    let b = quote!(
+        pub const GOODBYE_NAMED: &str = "Ciao!";
+    );
+
+    let together = quote!(
+        #contents
+        #b
+    );
+    let common_module = generate_named_module("common", &together);
+
+    // FIXME remove HELLO_V8 placeholder
     quote!(
         pub mod v8 {
-            // pub mod common;
-
-            // #(pub mod #families; )*
-
             pub const HELLO_V8: &str = "Hello from v8";
 
-            #common_mod
+            #common_module
+
+            // #(pub mod #families; )*
         }
     )
 }
 
-fn generate_common_module() -> TokenStream {
-    let aap = TokenStream::from_str("// dit is wat code").unwrap();
+fn generate_named_module(name: &str, contents: &TokenStream) -> TokenStream {
+    let name_literal = format_ident!("{name}");
     quote!(
-        pub mod common {
-            #aap
-            pub const HELLO_COMMON: &str = "Hello from common";
+        pub mod #name_literal {
+            #contents
         }
     )
 }
--- dis-rs/src/bin/v8.rs
+++ dis-rs/src/bin/v8.rs
@@ -3,5 +3,5 @@ use dis_rs::v8::common;
 
 fn main() {
     println!("v8 - {}", v8::HELLO_V8);
-    println!("Common - {}", common::HELLO_COMMON);
+    println!("Common - {}", common::HELLO_NAMED);
 }
